ff0a6e03b46659db0f5c60aa448a28d0b23b4028
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2021-01-30 14:41:56 -0500

    Added excercise questions

---
+++ pythonBasics2.py
@@ -0,0 +1,38 @@
+# Python Activtiy
+#
+# Fill in the code for the functions below.
+# The starter code for each function includes a 'return'
+# which is just a placeholder for your code.
+
+
+# Part A. fizz_buzz 
+# Define a function count_threes(n) that takes an int and 
+# returns the number of multiples of 3 in the range from 0
+# to n.
+
+def count_threes(n):
+  # YOUR CODE HERE
+
+  return
+
+
+# Part B. longest_consecutive_repeating_charater 
+# Define a function longest_consecutive_repeating_character(s) that takes
+# a string s and returns the character that has the longest consecutive repeat.
+def longest_consecutive_repeating_charater(s):
+  # YOUR CODE HERE
+
+  return
+
+
+# Part C. is_palindrome
+# Define a function is_palindrome(s) that takes a string s
+# and returns whether or not that string is a palindrome.
+# A palindrome is a string that reads the same backwards and 
+# forwards. Treat capital letters the same as lowercase ones
+# and ignore spaces.
+def is_palindrome(s):
+  # YOUR CODE HERE
+
+  return
+
